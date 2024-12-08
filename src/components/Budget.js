f122da60889fd142f538e92bae5630075b09316c
betsegaw-hp <betse53@gmail.com>
2024-12-08 08:46:21 -0500

    initial commit; working app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,25 @@
 
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    const handleBudgetSet = (e) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget
+        })
+    }
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<button type="button" id="new_Budget" onClick={handleBudgetSet} >SET</button>
+</div>
+    );
+};
+export default Budget;
